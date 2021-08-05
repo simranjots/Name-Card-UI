44d9482582db7a3c245aac1f655fd72b43414f10
simranjot singh <32786222+simranjots@users.noreply.github.com>
2021-08-05 12:45:21 -0400

    Initial Commit

---
+++ SimranjotCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimranjotCard
+//
+//  Created by Simranjot Singh Bagga on 2021-08-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
