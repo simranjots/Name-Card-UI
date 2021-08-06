524173da990ed5ea977c369f495897cfa51b2de1
simranjot singh <32786222+simranjots@users.noreply.github.com>
2021-08-06 02:03:57 -0400

    UI Design

--- SimranjotCard/ContentView.swift
+++ SimranjotCard/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("simranjot")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white,lineWidth: 5))
+                Text("Simranjot Singh")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+1 647-450-6358", imageName: "phone.fill")
+                InfoView(text: "simranjot@gmail.com", imageName: "envelope.fill")
+               
+            }
+                
+        }
     }
 }
 
@@ -19,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ SimranjotCard/InfoView.swift
@@ -0,0 +1,31 @@
+//
+//  InfoView.swift
+//  SimranjotCard
+//
+//  Created by Simranjot Singh Bagga on 2021-08-05.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text:String
+    let imageName:String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text).foregroundColor(.black)
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
